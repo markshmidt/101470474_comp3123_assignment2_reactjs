74e7feffb032060d988657d1842a53e04ec01ab2
Mariia <masha.shmidt.04@gmail.com>
2025-11-25 16:30:42 -0500

    fixes in backend implementation

--- backend/server.js
+++ backend/server.js
@@ -28,5 +28,5 @@ app.use("/api/employees", require("./routes/employeeRoutes"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Backend running on port ${PORT}`);
+  console.log(`Backend running on port ${PORT}`);
 });
